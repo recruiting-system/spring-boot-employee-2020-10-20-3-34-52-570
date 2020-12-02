5f815684dca727f0571f35a259cbf9b3a9f11de3
Ray Lam <ray.lam@oocl.com>
2020-12-02 15:16:23 +0800

    [SpringBoot Employee][Ray Lam] feat: create add PostMapping add employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,9 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,5 +14,11 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getEmployeesList(){
         return employees;
+    }
+
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employeeCreate) {
+        employees.add(employeeCreate);
+        return employeeCreate;
     }
 }
