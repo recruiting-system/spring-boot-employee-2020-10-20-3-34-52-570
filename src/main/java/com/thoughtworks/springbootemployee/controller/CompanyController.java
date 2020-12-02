eff00ed94bf1428730b71ca59761643bbfa64035
Ernest Cheung <ernest.cheung@oocl.com>
2020-12-02 16:20:21 +0800

    [CompanyController][Ernest] Feat: added getAll API for company

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,11 +1,22 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
+    private List<Company> companyList = new ArrayList<>();
 
+    @GetMapping
+    public List<Company> getAll() {
+        return this.companyList;
+    }
 
 }
