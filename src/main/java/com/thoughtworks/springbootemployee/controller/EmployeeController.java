fdaac9b1039cb92cf1f56dfe62ba82ab091e99c2
Ernest Cheung <ernest.cheung@oocl.com>
2020-12-02 14:57:41 +0800

    [EmployeeController][Ernest] Added getAll API

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,10 +1,20 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    private List<Employee> employeeList = new ArrayList<>();
 
+    @GetMapping
+    public List<Employee> getAll() {
+        return this.employeeList;
+    }
 }
